24cec7c563f5a31901c004a9cac41488251535e3
Garrett Serack <garretts@microsoft.com>
2011-04-28 14:03:24 -0700

    updated buildinfo for pTK

--- COPKG/build.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-REM builds from the Visual Studio command prompt just fine:
-cl /O2 /Tc *.c /DHAVE_STRERROR /link /out:grep.exe
-
--- COPKG/clean.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Cleaning project directory.
-
-erase /q *.obj > nul 2> nul
-erase /q *.exe > nul 2> nul
